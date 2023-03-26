db6011a19d77a225e7e2e4db154b4675ecd59109
Irzam Ijaz <122387136+irzamijaz@users.noreply.github.com>
2023-03-26 11:30:36 +0100

    modified:   package-lock.json 	modified:   src/pages/Error.jsx

--- blog-website/src/pages/Error.jsx
+++ blog-website/src/pages/Error.jsx
@@ -6,7 +6,7 @@ const Error = () => {
         <>
             <Box className='error-page page'>
                 <h1>Opps!</h1>
-                <h1>Error 404, Page Not Found....</h1>
+                <h1>Error 404,  Found....</h1>
             </Box>
         </>
     )
