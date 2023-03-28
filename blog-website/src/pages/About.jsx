e4b4af1490d4b43fd0f8378574eaae82c68a1f77
ChHammad4 <itzhammad8962@gmail.com>
2023-03-28 22:21:45 +0500

    minir change

--- blog-website/src/pages/About.jsx
+++ blog-website/src/pages/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
     return (
         <>
             <Box className='about-page page'>
-                <h1>About Page</h1>
+                <h1>About...</h1>
             </Box>
         </>
     )
