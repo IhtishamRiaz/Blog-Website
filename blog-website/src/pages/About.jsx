598a0855a28db451a1c827eb96740133cc844f38
ChHammad4 <itzhammad8962@gmail.com>
2023-03-29 20:50:02 +0500

    about sec-3

--- blog-website/src/pages/About.jsx
+++ blog-website/src/pages/About.jsx
@@ -20,7 +20,6 @@ const About = () => {
                         <div>
                             <button className='btn' type="text">OUR LEADERSHIPS</button>
                             <button className='btn-2' type="text">OUR BELIFES</button>
-
                         </div>
                     </div>
                 </div>
@@ -34,7 +33,7 @@ const About = () => {
                             this: 'Love your neighbor as yourself.' There is no commandment <br />
                             greater than these" (Mark 12:29-31).
                         </p>
-                    </div>
+                   
                     <div className='section-2-para2'>
                         <p className='section-2-para'>Plan a visit</p>
                         <p className='section-2-para'>Find a campus</p>
@@ -46,11 +45,28 @@ const About = () => {
                         <YouTubeIcon className='sec-icon2' color='black' fontSize='large' />
                     </div>
                 </div>
-                <div className='Sec-3'>
-                    <div>
-                        {/* <img className='img' src={img}/> */}
-                    </div>
+                <div className='about-video'>
+                <iframe width="440" height="280" src="https://www.youtube.com/embed/0h3FaS4E-7U" title="YouTube 
+                  video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted- 
+                   media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+               </div>
+             </div>
+             <div className='sec-3-main'>
+                <div className='sec-3-img'>
+                <img className='img' src={img} alt=''/>
                 </div>
+                <div className='sec-3-para'>
+                    <h3 className='sec-3-heading'>Robert Wachs, Lead Pastor</h3>
+                <p className='sec-3-paragraph'>Roberts Wachs is the Lead Pastor at Newbreak Church. When he is 
+                    isn't speaking at our <br />
+                     Tierrasanta campus, he is visiting our other campuses with his wife Lisa in Scripps Ranch  
+                      and <br /> Ocean Beach, encouraging their pastors and empowering their people. He's been 
+                       at Newbreak <br /> Church for more than 20 years and is excited for the future, as we 
+                        connect people with God <br /> through authentic relationships to serve communities. 
+                           </p>
+                </div>
+             </div>
+
 
             </Box>
         </>
