627227d5de7fad675551829ae42f778374d08ed7
IhtishamRiaz <riaz4u72nb+github@gmail.com>
2023-04-12 22:01:38 +0500

    ihtisham56764

--- blog-website/src/components/Footer.jsx
+++ blog-website/src/components/Footer.jsx
@@ -1,36 +1,21 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 const Footer = () => {
     return (
         <>
             <footer>
-                <div className="footer-container my-container">
-                    <div className="footer-col">
-                        <h1>Links</h1>
-                        <ul>
-                            <li><Link to='./'>Home</Link ></li>
-                            <li><Link to='./about'> About</Link ></li>
-                            <li><Link to='./policy'> Policy</Link ></li>
-                            <li><Link to='./contact'> Contact us</Link ></li>
-                        </ul>
+                <div className="my-container">
+                    <div className="footer-left">
+                        <p>@ Discovery Blogs</p>
+                        <p>10635 SCRIPPS RANCH BLVD. SUITE H SAN DIEGO, CA 92131 | (858) 576-0007</p>
                     </div>
-                    <div className="footer-col">
-                        <h1>Follow us</h1>
-                        <ul>
-                            <li><a href="https://facebook.com" target='_blank' rel="noreferrer">Facebook</a></li>
-                            <li><a href="https://linkedin.com" target='_blank' rel="noreferrer">Linkedin</a></li>
-                            <li><a href="https://instagram.com" target='_blank' rel="noreferrer">Instagram</a></li>
-                            <li><a href="https://youtube.com" target='_blank' rel="noreferrer">Youtube</a></li>
-                        </ul>
-                    </div>
-                    <div className="footer-col">
-                        <h1>Contact us</h1>
-                        <ul>
-                            <li><a href="https://goo.gl/maps/QjL39k9PJDyN9aPP9" target='_blank' rel="noreferrer">198 West 21th Street,<br />Suite 721 New York NY 10016</a></li>
-                            <li><a href='tel:+123 456 789'>+123 456 789</a></li>
-                            <li><a href='mailto:info@sample.com'>info@sample.com</a></li>
-                        </ul>
+
+                    <div className="footer-right">
+                        <div className="footer-icons">
+                            <i class="fa-brands fa-facebook-f"></i>
+                            <i class="fa-brands fa-youtube"></i>
+                            <i class="fa-brands fa-instagram"></i>
+                        </div>
                     </div>
                 </div>
             </footer>
